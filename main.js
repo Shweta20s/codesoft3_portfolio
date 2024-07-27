035b53152706544e54d88019d90943854e6216ec
shivi y <shivy9933@gmail.com>
2024-07-27 09:34:20 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+const scrollers = document.querySelectorAll(".scroller");
+
+// If a user hasn't opted in for recuded motion, then we add the animation
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    // add data-animated="true" to every `.scroller` on the page
+    scroller.setAttribute("data-animated", true);
+
+    // Make an array from the elements within `.scroller-inner`
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    // For each item in the array, clone it
+    // add aria-hidden to it
+    // add it into the `.scroller-inner`
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
+
+
+
+window.onscroll = () =>
+{
+  let header=document.querySelector('.nav')
+  let logo=document.querySelector('.headline')
+  if(window.scrollY>=1240){
+    logo.classList.add('color');
+    header.classList.add('sticky');
+  }
+  else{
+    logo.classList.remove('color');
+    header.classList.remove('sticky');
+  }
+}
+
+
+
